15f518af5effad75f65994735f9dd86698ab327e
Makhmutov Vladimir <mahmutov.vladimir@urbantelematics.com>
2022-10-10 17:00:13 +0700

    Начало работы перед новым бекендом

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import LeftSide from "./components/LeftSide/LeftSide";
 import RightSide from "./components/RightSide/RightSide";
 import './App.scss';
 
-const ws = new WebSocket('ws://192.168.100.225:23245')
+// const ws = new WebSocket('ws://192.168.100.225:23245')
+const ws = new WebSocket('ws://194.85.217.98:23245')
 
 function App() {
     const [routeInfo, setRouteInfo] = useState([])
